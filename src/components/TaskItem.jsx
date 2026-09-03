aca1ff6022d449859ab017a85e3aeac8716f4fa4
Bruno Escritório <carecsnay1@gmail.com>
2026-09-03 11:30:59 -0300

    feat: ensure TaskItem changes depending on task status

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,7 +1,9 @@
+import { CheckIcon, DetailsIcon, LoaderIcon } from '../assets/icons';
+
 const TaskItem = ({ task }) => {
   const statusVariants = {
-    done: 'bg-[#00ADB5]/10 text-[#00ADB5]',
-    in_progress: 'bg-[#ffaa04]/10 text-[#ffaa04]',
+    done: 'bg-[#00ADB5] text-[#00ADB5]',
+    in_progress: 'bg-[#ffaa04] text-[#ffaa04]',
     not_started: 'bg-[#35383e]/10 text-[#35383e]',
   };
 
@@ -10,9 +12,28 @@ const TaskItem = ({ task }) => {
 
   return (
     <div
-      className={`flex items-center gap-2 rounded px-4 py-3 text-sm ${currentVariant}`}
+      className={`flex items-center justify-between gap-2 rounded bg-opacity-10 px-4 py-3 text-sm ${currentVariant}`}
     >
-      <span className="text-sm font-medium">{task.title}</span>
+      <div className="flex items-center justify-center gap-2">
+        <label
+          className={`relative flex h-7 w-7 cursor-pointer items-center justify-center rounded-lg ${currentVariant}`}
+        >
+          <input
+            type="checkbox"
+            checked={task.status === 'done'}
+            className="absolute h-full w-full cursor-pointer opacity-0"
+          />
+          {task.status === 'done' && <CheckIcon />}
+          {task.status === 'in_progress' && (
+            <LoaderIcon className="animate-spin" />
+          )}
+        </label>
+        {task.title}
+      </div>
+
+      <a href="/#" className="transition-all hover:opacity-75">
+        <DetailsIcon />
+      </a>
     </div>
   );
 };
