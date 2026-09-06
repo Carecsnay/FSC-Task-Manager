5e85188c7a3ef31de137a75c9b06a9bfd1f8066e
Bruno Asus <carecsnay1@gmail.com>
2026-09-06 09:39:09 -0300

    feat: add new variant and sizes on component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,31 @@
-const Button = ({ children, icon, variant = 'primary', ...rest }) => {
+// o Rest basicamente pega todas as propriedades, exceto as que eu desestruturei tipo a className
+const Button = ({
+  children,
+  icon,
+  variant = 'primary',
+  size = 'small',
+  className,
+  ...rest
+}) => {
   const variants = {
     primary: 'bg-[#00ADB5] text-[#E6F7F8]',
+    secondary: 'bg-[#EEEEEE] text-[#35383E]',
     ghost: 'bg-transparent text-[#35383E]',
   };
 
+  const sizes = {
+    small: 'py-1 text-xs',
+    medium: 'py-2 text-sm',
+  };
+
+  // Garante o caso passem uma prop inválida || valor default (fallback)
+  const selectedVariant = variants[variant] || variants.primary;
+  const selectedSize = sizes[size] || sizes.small;
+
   return (
     <button
-      className={`${variants[variant] || variants.primary} flex items-center gap-1 rounded px-3 py-1.5 text-xs font-semibold transition hover:opacity-60`}
-      {...rest} //todas as propriedades de um botão
+      className={`${selectedVariant} ${selectedSize} flex items-center justify-center gap-2 rounded-lg px-3 font-semibold transition hover:opacity-80 active:opacity-90 ${className}`}
+      {...rest}
     >
       {children}
       {icon}
