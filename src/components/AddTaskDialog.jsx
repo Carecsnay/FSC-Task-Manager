23f29048859376e56526cbee90cc45fc1833515e
Bruno Asus <carecsnay1@gmail.com>
2026-09-10 20:29:50 -0300

    feat: add ensure shows alert when form is empty

--- src/components/AddTaskDialog.jsx
+++ src/components/AddTaskDialog.jsx
@@ -13,6 +13,9 @@ const AddTaskDialog = ({ isOpen, handleClose, handleSubmit }) => {
   const [description, setDescription] = useState();
 
   const handleSaveClick = () => {
+    if (!title.trim() || !time.trim() || !description.trim()) {
+      return alert('Preencha todos os campos!');
+    }
     handleSubmit({
       id: v4(),
       title,
